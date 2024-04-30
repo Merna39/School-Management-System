1b93bb77116fdf44e08c0847266588126d5df1b8
Ali Adel Ali <aliafel357@gmail.com>
2024-05-01 01:18:24 +0300

    edit sql query

--- admin/student/attendance.php
+++ admin/student/attendance.php
@@ -24,67 +24,66 @@
 <section class="content">
     <div class="container-fluid">
 
-        <?php 
-            // $std_id = isset($_GET['std_id']) ? $_GET['std_id'] : '';
-            $usermeta = get_user_metadata($std_id);
+        <?php
+        // $std_id = isset($_GET['std_id']) ? $_GET['std_id'] : '';
+        $usermeta = get_user_metadata($std_id);
         ?>
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Student Detail</h3>
-                </div>
-                <div class="card-body">
-                    <strong>Name: </strong> <?php echo get_users(array('id' => $std_id))[0]->name ?> <br>
-                    <strong>Class: </strong> <?php echo $usermeta['class'] ?>
-
-                </div>
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Student Detail</h3>
             </div>
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Attendance</h3>
-                </div>
-                <div class="card-body">
-                    <table class="table table-bordered">
+            <div class="card-body">
+                <strong>Name: </strong> <?php echo get_users(array('id' => $std_id))[0]->name ?> <br>
+                <strong>Class: </strong> <?php echo $usermeta['class'] ?>
+
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Attendance</h3>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
                     <thead>
-                            <tr>
-                                <td>S.no</td>
-                                <td>Date</td>
-                                <td>Status</td>
-                                <td>Singin Time</td>
-                                <td>Singout Time</td>
-                            </tr>
-                        </thead>
-                        <tbody>
+                        <tr>
+                            <td>S.no</td>
+                            <td>Date</td>
+                            <td>Status</td>
+                            <td>Singin Time</td>
+                            <td>Singout Time</td>
+                        </tr>
+                    </thead>
+                    <tbody>
 
                         <?php
-                            $current_month = strtolower(date('F'));
-                            $current_year = date('Y');
-                            $sql = "SELECT * FROM `attendance` WHERE `attendance_month` = '$current_month' AND year(current_session) = $current_year";
+
+                        $sql = "SELECT * FROM attendance WHERE attendance_month = date('F') AND year(current_session) = date('Y');";
 
 
-                            $query = mysqli_query($db_conn, $sql);
+                        $query = mysqli_query($db_conn, $sql);
 
-                            $row = mysqli_fetch_object($query);
-                  
-                            // echo '<pre>';
-                            // print_r($row);
-                            // echo '</pre>';
+                        $row = mysqli_fetch_object($query);
 
-                            
-                            foreach(unserialize($row->attendance_value) as $date => $value){ ?>
-                              <tr>
-                                   <td><?php echo $date;?></td>
-                                   <td><?php echo ($value['signin_at'])? 'Present' : 'Absent';?></td>
-                                   <td><?php echo ($value['signin_at'])? date('d-m-yyy h:i:s', $value['signin_at']) : '';?></td>
-                                   <td><?php echo ($value['signout_at'])? date('d-m-yyy h:i:s', $value['signout_at']) : '';?></td>
-                               </tr>
+                        echo '<pre>';
+                        print_r($row);
+                        echo '</pre>';
 
-                               <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
+
+                        foreach (unserialize($row->attendance_value) as $date => $value) { ?>
+                            <tr>
+                                <td><?php echo $date; ?></td>
+                                <td><?php echo ($value['signin_at']) ? 'Present' : 'Absent'; ?></td>
+                                <td><?php echo ($value['signin_at']) ? date('d-m-yyy h:i:s', $value['signin_at']) : ''; ?></td>
+                                <td><?php echo ($value['signout_at']) ? date('d-m-yyy h:i:s', $value['signout_at']) : ''; ?></td>
+                            </tr>
+
+                        <?php } ?>
+                    </tbody>
+                </table>
             </div>
+        </div>
 
-       
+
     </div><!--/. container-fluid -->
 </section>
 <!-- /.content -->
