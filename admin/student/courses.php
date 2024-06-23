983972b7734becc9a8ac05c7a95dbe0d2da0fdbc
MadonnaMelad <120129713+MadonnaMelad@users.noreply.github.com>
2024-06-23 19:11:16 +0300

    last edit

--- admin/student/courses.php
+++ admin/student/courses.php
@@ -74,11 +74,11 @@ if (isset($_POST['submit'])) {
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0 text-dark">Manage Courses </h1>
+        <h1 class="m-0 text-dark"> Courses </h1>
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="#">Admim</a></li>
+          <li class="breadcrumb-item"><a href="#">Student</a></li>
           <li class="breadcrumb-item active">Courses</li>
         </ol>
       </div><!-- /.col -->
